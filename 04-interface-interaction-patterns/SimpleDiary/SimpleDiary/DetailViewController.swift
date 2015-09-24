aa155ce463eb768f9f1dad94d778936601d3dc40
Anastasiia Babicheva <babicheva.ps07a@gmail.com>
2015-09-24 21:38:48 +0300

    Checkboxes on Settings screen implemented

---
+++ 04-interface-interaction-patterns/SimpleDiary/SimpleDiary/DetailViewController.swift
@@ -0,0 +1,117 @@
+//
+//  DetailViewController.swift
+//  SimpleDiary
+//
+//  Created by ADM on 9/18/15.
+//  Copyright (c) 2015 Buypass. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var titleText: UITextField!
+
+    @IBOutlet weak var entryText: UITextField!
+    
+    @IBOutlet weak var weatherSegments: UISegmentedControl!
+    
+    var managedObjectContext: NSManagedObjectContext? = nil
+    
+    var detailItem: DiaryRecord? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+    
+    func didWeatherChange() {
+        NSLog("weather changed: %d", self.weatherSegments.selectedSegmentIndex)
+        setBackgroundColor(self.weatherSegments.selectedSegmentIndex)
+    }
+    
+    func setBackgroundColor(segmentIndex: Int) {
+        let weather: Weather = Weather(rawValue: segmentIndex)!
+        switch weather {
+        case Weather.Cloudy:
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-cloudy")!)
+            weatherSegments.tintColor = UIColor.whiteColor()
+        case Weather.Rain:
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-rainy")!)
+            weatherSegments.tintColor = UIColor.blackColor()
+        case Weather.Sunny:
+            self.view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-sunny")!)
+            weatherSegments.tintColor = UIColor.blueColor()
+        }
+    }
+    
+    func saveDiaryRecord() {
+        NSLog("save diary record")
+        if let detail = self.detailItem {
+            if let title = self.titleText {
+                detail.setValue(title.text, forKey: "name")
+            }
+            
+            if let body = self.entryText {
+                detail.setValue(body.text, forKey: "body")
+            }
+            
+            if let weather = self.weatherSegments {
+                detail.setValue(weather.selectedSegmentIndex , forKey: "weather")
+            }
+        }
+        
+        do {
+            try self.managedObjectContext!.save()
+        } catch {
+            fatalError("Failure to save context: \(error)")
+        }
+        
+        self.navigationController?.popToRootViewControllerAnimated(true)
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: DiaryRecord = self.detailItem {
+            self.navigationItem.title = detail.formattedDateShort()
+            
+            if let title = self.titleText {
+                title.text = detail.name
+            }
+            
+            if let body = self.entryText {
+                body.text = detail.body
+            }
+            
+            if let weather = self.weatherSegments {
+                weather.selectedSegmentIndex = detail.valueForKey("weather") as! Int
+                setBackgroundColor(weather.selectedSegmentIndex)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+        
+        let doneButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Done, target: self, action: "saveDiaryRecord")
+        self.navigationItem.rightBarButtonItem = doneButtonItem
+        
+        self.weatherSegments.addTarget(self, action: "didWeatherChange", forControlEvents: UIControlEvents.ValueChanged)
+        
+        if(self.detailItem == nil) {
+            self.detailItem = NSEntityDescription.insertNewObjectForEntityForName("DiaryRecord", inManagedObjectContext: managedObjectContext!) as? DiaryRecord
+            self.detailItem?.setValue(NSDate(), forKey: "creationDate")
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 04-interface-interaction-patterns/SimpleDiary/SimpleDiary/MasterViewController.swift
@@ -0,0 +1,165 @@
+//
+//  MasterViewController.swift
+//  SimpleDiary
+//
+//  Created by ADM on 9/18/15.
+//  Copyright (c) 2015 Buypass. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class MasterViewController: UITableViewController, SettingsViewDelegate {
+    
+    @IBOutlet var recordsTable: UITableView!
+    
+    weak var detailViewController: DetailViewController?
+
+    var objects = [DiaryRecord]()
+    
+    var lastClickedRowIndex: Int = -1
+    
+    var dateSetting: DateSetting = DateSetting.ShortFormat
+    
+    var managedObjectContext: NSManagedObjectContext? = nil
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "settings"), style: UIBarButtonItemStyle.Plain, target: self, action: "openSettings")
+
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
+        
+        setupObjects()
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        NSLog("in viewDidAppear")
+        if(self.lastClickedRowIndex != -1) {
+            // We returned from detail controller
+            if let detailVC = self.detailViewController {
+                if(self.lastClickedRowIndex > objects.endIndex) {
+                    objects.append(detailVC.detailItem!)
+                } else {
+                    objects[lastClickedRowIndex] = detailVC.detailItem!
+                }
+                if let table = recordsTable {
+                    objects.sortInPlace({(dr1: DiaryRecord, dr2: DiaryRecord) -> Bool
+                        in return (dr1.creationDate!.compare(dr2.creationDate!) == NSComparisonResult.OrderedDescending)})
+                    table.reloadData()
+                }
+                
+                self.lastClickedRowIndex = -1
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupObjects() {
+        let moc = self.managedObjectContext
+        let employeesFetch = NSFetchRequest(entityName: "DiaryRecord")
+        
+        do {
+            let records = try moc!.executeFetchRequest(employeesFetch) as! [DiaryRecord]
+            objects = records
+        } catch {
+            fatalError("Failed to fetch records: \(error)")
+        }
+        
+        objects.sortInPlace({(dr1: DiaryRecord, dr2: DiaryRecord) -> Bool
+            in return ((dr1.valueForKey("creationDate") as! NSDate).compare(dr2.valueForKey("creationDate") as! NSDate) == NSComparisonResult.OrderedDescending)})
+    }
+
+    func insertNewObject(sender: AnyObject) {
+        self.lastClickedRowIndex = objects.endIndex + 1
+        self.detailViewController = self.storyboard?.instantiateViewControllerWithIdentifier("detailViewController") as? DetailViewController;
+        self.detailViewController?.managedObjectContext = self.managedObjectContext
+        self.navigationController?.pushViewController(self.detailViewController!, animated: true);
+    }
+    
+    func openSettings() {
+        NSLog("in openSettings")
+        let settingsNavController = self.storyboard?.instantiateViewControllerWithIdentifier("settingsNavController") as! UINavigationController
+        
+        let settingsViewController = settingsNavController.topViewController as! SettingsViewController
+        settingsViewController.delegate = self
+        settingsViewController.selectedSetting = dateSetting
+        settingsViewController.modalPresentationStyle = UIModalPresentationStyle.CurrentContext
+        settingsViewController.modalTransitionStyle = UIModalTransitionStyle.CoverVertical
+        
+        presentViewController(settingsNavController, animated: true, completion: nil)
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                self.lastClickedRowIndex = indexPath.row
+                let object = objects[indexPath.row]
+                self.detailViewController = (segue.destinationViewController as! DetailViewController)
+                self.detailViewController?.managedObjectContext = self.managedObjectContext
+            (segue.destinationViewController as! DetailViewController).detailItem = object
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) 
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object.name
+        
+        
+        let date: String
+        self.dateSetting.rawValue == DateSetting.ShortFormat.rawValue ? (date = object.formattedDateShort()) : (date = object.formattedDateLong())
+        cell.detailTextLabel!.text = date
+        
+        
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            objects.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+        }
+    }
+    
+    func didChooseSetting(selectedSetting: DateSetting?) {
+        if let setting = selectedSetting {
+            self.dateSetting = setting
+            if let table = self.recordsTable {
+                table.reloadData()
+            }
+        }
+    }
+
+}
+
